06c5de4499a4afc1ded122dadcaa4bf1855b58d0
Oussama <oussamay713@gmail.com>
2024-11-18 16:45:05 +0100

    explicit filter for books by this author

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -5,10 +5,11 @@ from relationship_app.models import Author, Book, Library, Librarian
 author_name = "Some Author Name"  # Replace with the actual author's name
 try:
     author = Author.objects.get(name=author_name)
-    books_by_author = author.books.all()  # Use the related_name 'books' for reverse lookup
+    books_by_author = Book.objects.filter(author=author)  # Use explicit filter for books by this author
     print(f"Books by {author_name}: {[book.name for book in books_by_author]}")
 except Author.DoesNotExist:
     print(f"Author with name '{author_name}' does not exist.")
+
 
 # List all books in a library
 library_name = "Some Library Name"  # Replace with the actual library name
