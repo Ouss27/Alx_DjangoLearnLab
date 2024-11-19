c2062a7f4ec5705983e016341951626e8e527f66
Oussama <oussamay713@gmail.com>
2024-11-19 23:31:14 +0100

    templates added

--- django-models/LibraryProject/settings.py
+++ django-models/LibraryProject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django-models.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / "templates" ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
