ccc3cfd0b323d56f4f334e08d33970c3e4f55cfd
Oussama <oussamay713@gmail.com>
2024-11-19 23:07:41 +0100

    class SignUpView deleted for test

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -22,8 +22,5 @@ class LibraryDetailView(DetailView):
     template_name='relationship_app/library_detail.html'
     context_object_name='library'
 
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/register.html'
+
     
