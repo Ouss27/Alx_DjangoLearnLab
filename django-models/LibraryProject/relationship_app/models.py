dfc2b6ab6a2a19a0a97f62f020816e72f7e424ec
Oussama <oussamay713@gmail.com>
2024-12-04 09:15:43 +0100

    all tests done

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -35,16 +35,25 @@ class Librarian(models.Model):
     def __str__(self):
         return self.name
     
+#UserProfile model with a role  
 class UserProfile(models.Model):
     ROLE_CHOICES = [
         ('Admin', 'Admin'),
         ('Librarian', 'Librarian'),
         ('Member', 'Member'),
     ]
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Member')
 
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+#Django Signal to Automatically Create Profiles
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
