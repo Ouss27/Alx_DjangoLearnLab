dcc72091e04077619caaecaaf6146f2d3ebc8ddb
Oussama <oussamay713@gmail.com>
2024-12-24 15:45:02 +0100

    Configured URL Patterns

--- advanced_features_and_security/LibraryProject/LibraryProject/urls.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include  # Import include to reference app URLs
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('LibraryProject.relationship_app.urls')),  # Include URLs from our relationship_app
+    path('', include('bookshelf.urls')),  # Include URLs from our bookshelf app
 ]
