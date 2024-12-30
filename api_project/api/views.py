829e5426ca7dd85acacc993e396a7a982d9b6411
Oussama <oussamay713@gmail.com>
2024-12-30 19:48:55 +0100

    defined permission classes/ Donne all tests

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from rest_framework import viewsets, generics, permissions
+from rest_framework import viewsets, generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
 
 
 class BookList(generics.ListAPIView):
@@ -11,4 +12,6 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all().order_by("title")
     serializer_class = BookSerializer
-   # permission_classes = [permissions.IsAuthenticated]
+   # permission_classes = [IsAuthenticated] ## Only authenticated users can access this view
+   # permission_classes = [IsAdminUser] ## Only admin users can access this view
+    permission_classes = [IsAuthenticatedOrReadOnly] ## read-only access to unauthenticated users and full access to authenticated users
