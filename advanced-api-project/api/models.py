de1f122928e7886c4703d985673a46ebebc4c7a4
Oussama <oussamay713@gmail.com>
2025-01-01 14:38:22 +0100

    Seted Up Generic Views

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -6,7 +6,13 @@ from datetime import datetime
 class Author(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     publication_year = models.IntegerField(max_length=4, default=datetime.now().year)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return self.title
