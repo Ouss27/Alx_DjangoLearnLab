fa9c46ffd9227780c57674afc3948e7762c8c296
Oussama <oussamay713@gmail.com>
2025-01-01 18:17:36 +0100

    url patts updated to support pk

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name= 'books_list'),
     path('book/<int:pk>/', BookDetailView.as_view(), name= 'book_detail'),
     path('books/create/', BookCreateView.as_view(), name= 'books_create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name= 'books_update'),
-    path('books/delete/', BookDeleteView.as_view(), name= 'books_delete'),
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name= 'books_update'),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name= 'books_delete'),
 ]
 
