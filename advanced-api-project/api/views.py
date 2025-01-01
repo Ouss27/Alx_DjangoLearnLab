9215485603d9faaae9d235f553ec5234f44ee109
Oussama <oussamay713@gmail.com>
2025-01-01 15:24:20 +0100

    Implemented permissions

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,31 +2,46 @@ from django.shortcuts import render
 from rest_framework import generics
 from api.models import Book
 from api.serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
 
 
 # ListView: Retrieve all books
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]  # Allows unauthenticated read-only access
+
 
 # DetailView: Retrieve a single book by ID
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]  # Allows unauthenticated read-only access
+
 
 # CreateView: Add a new book
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Only authenticated users can create books
+    # Custom validation is already provided in Serializer class
+
+
+
 
 # UpdateView: Modify an existing book
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Only authenticated users can update books
+
 
 # DeleteView: Remove a book
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Only authenticated users can delete books
+
 
     
