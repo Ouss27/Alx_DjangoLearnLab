2a79db2361e06d31ecbbe1547411275842fd4842
Oussama <oussamay713@gmail.com>
2025-01-01 14:54:35 +0100

    Defined url patterns v3

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('books/', BookListView.as_view, name= 'books_list'),
     path('book/<int:pk>/', BookDetailView.as_view, name= 'book_detail'),
     path('books/create/', BookCreateView.as_view, name= 'books_create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view, name= 'books_update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view, name= 'books_delete'),
+    path('books/update/', BookUpdateView.as_view, name= 'books_update'),
+    path('books/delete/', BookDeleteView.as_view, name= 'books_delete'),
 ]
 
