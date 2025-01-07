dd74e5425e0d97f48c7f5b820145b8afba073d0d
Oussama <oussamay713@gmail.com>
2025-01-07 19:54:18 +0100

    Included posts app URLs under the 'api/' path

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+     path('api/', include('posts.urls')),  # Include posts app URLs under the 'api/' path
+    path('api-auth/', include('rest_framework.urls')),  # For browsable API authentication
 ]
