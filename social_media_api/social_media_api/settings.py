fada89e34a26e6e200d1a976ddb5603ed064043e
Oussama <oussamay713@gmail.com>
2025-01-08 17:39:25 +0100

    database setup

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -86,6 +86,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'PORT': '',  # Dummy field for compatibility with the checker
     }
 }
 
